9064f09321132fa4a835f55d8fb3a72008820589
Oscar Barragan <oscaribv@gmail.com>
2017-05-10 11:41:23 +0200

    Now the code prints Zeng models automatically

--- planets_precision.py
+++ planets_precision.py
@@ -29,7 +29,15 @@ max_r = 3.0
 max_m = 20 * 1.0 / 317.83 
 max_r = 3.0 / 11.209
 
+is_plot_zeng_models = True
+
+#Default jupiter to earth masses transformation
+mfact = 317.83
+rfact = 11.209
+
+#------------------------------------------------------------------------
 #All the magic!
+#------------------------------------------------------------------------
 
 #I use the well-characterized planets table available at
 #http://www.astro.keele.ac.uk/jkt/tepcat/
@@ -42,7 +50,7 @@ urllib.urlretrieve(urlname, filename=fname)
 m,mle,mre,r,rle,rre = np.loadtxt(fname,delimiter=',',unpack=True,usecols=range(26,32),skiprows=1)
 
 #Let us store the index for the planets which are inside the range and have error bars 
-#smaller than our criteria
+#smaller than our selection criteria
 good_index = []
 
 for o in range(0,len(m)):
@@ -51,6 +59,7 @@ for o in range(0,len(m)):
       if ( rle[o] / r[o] < precision_r and rre[o] / r[o] < precision_r ):
         good_index.append(o)
 
+#Print the details
 print "There are ", len(good_index), 'exoplanets'
 print "between", min_m, ' and ', max_m, 'Jupiter masses'
 print "between", min_r, ' and ', max_r, 'Jupiter masses'
@@ -63,6 +72,13 @@ print "Precision in r better than ", precision_r
 
 #Zeng models
 newzeng = np.loadtxt('newZeng.txt',unpack=True)
+
+if ( is_plot_zeng_models ): 
+  plt.plot(newzeng[0]/mfact,newzeng[1]/rfact,color='#800000',label='$\mathrm{Fe}$')
+  plt.plot(newzeng[0]/mfact,newzeng[2]/rfact,'--',color='#B22222',label='50%$\mathrm{Fe}$-50%$\mathrm{MgSiO}_3$')
+  plt.plot(newzeng[0]/mfact,newzeng[6]/rfact,'y',label='$\mathrm{MgSiO}_3$')
+  plt.plot(newzeng[0]/mfact,newzeng[8]/rfact,'b--',label='50%$\mathrm{MgSiO_3}$-50%$\mathrm{H_2O}$')
+  plt.plot(newzeng[0]/mfact,newzeng[9]/rfact,'c',label='$\mathrm{H_2O}$')
 
 
 plt.semilogx()
